f300064440ce7501d3829b01cf90c475af1cd0cc
Franek Wojas <49079734+FranekDev@users.noreply.github.com>
2023-07-02 18:06:41 +0200

    Added ability to place Your move

--- main.js
+++ main.js
@@ -2,15 +2,21 @@ const gameBoard = document.querySelector('.gameBoard');
 const gameField = document.querySelectorAll('.field');
 
 const GameBoard = () => {
-    let board = ['X', 'X', 'X', 'X', 'O', 'O', 'X', 'O', 'O'];
+    let board = ['', '', '', '', '', '', '', '', ''];
 
     const render = () => {
         gameField.forEach((field, index) => {
             field.textContent = board[index];
         });
-    }
+    };
 
-    return { board, render };
+    const reset = () => {
+        board.forEach((field, index) => {
+            board[index] = '';
+        });
+    };
+
+    return { board, render, reset };
 };
 
 let Player = (move) => {
@@ -26,3 +32,51 @@ let Player = (move) => {
 const game = GameBoard();
 game.render();
 
+const GameController = (() => {
+
+    const winCases = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6] 
+    ];
+
+    const checkWinner = () => {
+        let winner = '';
+        return winner;
+    }
+
+    const resetGame = () => {
+        console.log('game reset');
+        game.reset();
+        game.render();
+    };
+
+    return { resetGame, checkWinner };
+
+})();
+
+let wasX = false;
+gameField.forEach((field) => {
+    field.addEventListener('click', () => {
+        if (!wasX && field.textContent == '') {
+            field.textContent = 'X';
+            wasX = true;
+        }
+        else if (wasX && field.textContent == '') {
+            field.textContent = 'O';
+            wasX = false;
+        }
+        GameController.checkWinner();
+    });
+});
+
+
+const resetButton = document.querySelector('.resetBtn');
+resetButton.addEventListener('click', () => {
+    GameController.resetGame();
+});
